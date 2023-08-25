2975833ddc237af028001960ebcb9cc6b92e1c98
Timo <timo.antoniak@btb.gmbh>
2023-08-25 08:43:25 +0200

    initial Commit

---
+++ src/main/java/de/timo/tutorial_ami/TutorialAmiApplication.java
@@ -0,0 +1,13 @@
+package de.timo.tutorial_ami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialAmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialAmiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/timo/tutorial_ami/TutorialAmiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.timo.tutorial_ami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialAmiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
